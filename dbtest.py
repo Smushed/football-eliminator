0e88f7a28bf7833cf7d84add406f03965902ada4
Smushed <smushedcode@gmail.com>
2019-04-24 22:02:44 -0500

    New Notes on where to start tomorrow

--- dbtest.py
+++ dbtest.py
@@ -3,15 +3,21 @@ import nfldb
 db = nfldb.connect()
 q = nfldb.Query(db)
 
-weeknum = 1
-
+# https://github.com/BurntSushi/nfldb/wiki/Aggregate-searching
 # Start here - https://pynative.com/python-postgresql-tutorial/
 
-q.game(season_year=2018, season_type='Regular')
-q.player()
+weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-for player in q.sort('passing_yds').limit(1).as_aggregate():
-    print player
+q.game(season_year=2018, season_type='Regular')
+for pp in q.sort('rushing_yds').limit(5).as_aggregate():
+    print pp.player, pp.rushing_yds
+
+
+# q.game(season_year=2018, season_type='Regular')
+# q.player()
+
+# for player in q.sort('passing_yds').limit(1).as_aggregate():
+#     print player
 
     # topQB = game.players.sort('passing_yds').limit(1)
     # print topQB
