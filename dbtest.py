4eb7033dcfaa56263562ac9ea50be06c33a2f0a6
Smushed <smushedcode@gmail.com>
2019-04-25 22:23:20 -0500

    Working with getting player stats

--- dbtest.py
+++ dbtest.py
@@ -1,32 +1,42 @@
 import nfldb
 
 db = nfldb.connect()
-q = nfldb.Query(db)
+search = nfldb.Query(db)
 
 # https://github.com/BurntSushi/nfldb/wiki/Aggregate-searching
 # Start here - https://pynative.com/python-postgresql-tutorial/
+# https://github.com/BurntSushi/nfldb/issues/35
 
-weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+# weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-q.game(season_year=2018, season_type='Regular')
-for pp in q.sort('rushing_yds').limit(5).as_aggregate():
-    print pp.player, pp.rushing_yds
-
-
-# q.game(season_year=2018, season_type='Regular')
-# q.player()
-
-# for player in q.sort('passing_yds').limit(1).as_aggregate():
-#     print player
-
-    # topQB = game.players.sort('passing_yds').limit(1)
-    # print topQB
+# q.game(season_year=2018, season_type='Regular', week=1, team="CHI")
+# for players in q.as_players():
+#     print players
 
 
-# for weeknum in range(17):
-#     player = q.sort('passing_yds').limit(1).as_aggregate()
-#     topplayers.append(player[0].player)
-#     weeknum = weeknum + 1
+def season_player_stats(full_name, season):
+    q = nfldb.Query(db)
 
-# for pp in q.sort('passing_yds').limit(10).as_aggregate():
-#     print pp.player, pp.passing_yds
+    q.game(season_year=season, season_type='Regular')
+    q.player(full_name=full_name)
+    pps = q.as_aggregate()
+
+    assert len(pps) < 2, "Make sure only one player was aggregated"
+    if len(pps) == 0:  # No players found to aggregate
+        return None
+    return pps[0]
+
+
+search.game(season_year=2018, season_type='Regular', week=1, team="CHI")
+for players in search.as_players():
+    print players.full_name, players.position
+    if players.position == 'TE':  # HOW DO I DO THIS???
+        stats = season_player_stats(players.full_name, 2018)
+        print players.full_name
+        for field in sorted(stats.fields):
+            print '%s: %s' % (field, getattr(stats, field))
+
+
+# stats = season_player_stats('Mitchell Trubisky', 2018)
+# for field in sorted(stats.fields):
+#     print '%s: %s' % (field, getattr(stats, field))
