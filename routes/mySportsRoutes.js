607358d42e3e28418c5c1762aa62372e6c7221a0
Smushed <smushedcode@gmail.com>
2019-05-18 15:29:20 -0500

    Adding a comment

--- routes/mySportsRoutes.js
+++ routes/mySportsRoutes.js
@@ -13,6 +13,8 @@ module.exports = app => {
         //This is a route which pulls in weekly data (should use params in the future)
         const weeklyPlayerData = await mySportsHandler.getWeeklyData(`2018-2019-regular`, week)
         //getWeeklyData returns all player data for that week in an array
+        //TODO Change this
+        //Currently if this runs while there is no new players to add the front end will break
         res.status(200).send(weeklyPlayerData)
     });
 }
