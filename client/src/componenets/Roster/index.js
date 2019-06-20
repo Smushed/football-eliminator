3ebaf96ff75ee5aeb1b72c773cea5635e551f6df
Smushed <smushedcode@gmail.com>
2019-06-20 07:15:43 -0500

    Trying to work with beautiful dnd

--- client/src/componenets/Roster/index.js
+++ client/src/componenets/Roster/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import { Row, Col } from 'reactstrap';
 
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 //Start here and use React Beautiful DND
 
 class Roster extends Component {
@@ -31,12 +31,47 @@ class Roster extends Component {
             console.log(`working`)
         }
     }
+    onBeforeDragStart = () => {
+        /*...*/
+    };
+
+    onDragStart = () => {
+        /*...*/
+    };
+    onDragUpdate = () => {
+        /*...*/
+    };
+    onDragEnd = () => {
+        // the only one that is required
+    };
 
     render() {
         return (
-            <div>
-                working
-            </div>
+            <DragDropContext
+                onBeforeDragStart={this.onBeforeDragStart}
+                onDragStart={this.onDragStart}
+                onDragUpdate={this.onDragUpdate}
+                onDragEnd={this.onDragEnd}
+            >
+                <Droppable droppableId="droppable-1" type="PERSON">
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+                            {...provided.droppableProps}
+                        >
+                            <h2>I am a droppable!</h2>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                    <Draggable draggableId="draggable-1" index={0}>
+                        {(provided, snapshot) => (
+                            <h4>My draggable 1</h4>
+                        )}
+                    </Draggable>
+                </Droppable>
+                <div>Hello world</div>
+            </DragDropContext>
         )
     }
 }
