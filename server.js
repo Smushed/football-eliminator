2d0530bec4657e3daf805c142e11d18af4b9f881
Smushed <smushedcode@gmail.com>
2020-07-14 14:57:25 -0500

    Working with heroku routes

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ require(`./routes/userRoutes`)(app);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get(`/*`, (req, res) => {
-  res.sendFile(path.join(__dirname, `../client/build/index.html`));
+app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, `./client/build/index.html`));
 });
 
 app.listen(PORT, () => {
