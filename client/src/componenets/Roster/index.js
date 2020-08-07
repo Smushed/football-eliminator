d55b5f9b89be1b887fc7ccd6ad24bef710898ac0
Smushed <smushedcode@gmail.com>
2020-08-07 10:45:38 -0500

    Roster Mobile Ready

--- client/src/componenets/Roster/index.js
+++ client/src/componenets/Roster/index.js
@@ -531,10 +531,18 @@ class Roster extends Component {
                         Change Week
                         <WeekSearch weekSelect={this.state.weekSelect} handleChange={this.handleChange} customSeasonWeekSearch={this.customSeasonWeekSearch} />
                     </div>
+                    <div className='searchRow'>
+                        Position Search
+                        <PositionSearch positionSelect={this.state.positionSelect} handleChange={this.handleChange} positionSearch={this.positionSearch} />
+                    </div>
+                    <div className='searchRow'>
+                        Player Search
+                        <PlayerSearch playerSearch={this.state.playerSearch} handleChange={this.handleChange} customPlayerSearch={this.customPlayerSearch} />
+                    </div>
                 </div>
                 <div className='rosterContainer'>
                     <div className='rosterCol'>
-                        <div className='searchRow largeScreenSearch'>
+                        <div className='searchRow largeScreenShow'>
                             Change Week
                             <WeekSearch weekSelect={this.state.weekSelect} handleChange={this.handleChange} customSeasonWeekSearch={this.customSeasonWeekSearch} />
                         </div>
@@ -548,7 +556,7 @@ class Roster extends Component {
                         />
                     </div>
                     <div className='rosterCol'>
-                        <div className='searchRow'>
+                        <div className='searchRow largeScreenShow'>
                             Position Search
                             <PositionSearch positionSelect={this.state.positionSelect} handleChange={this.handleChange} positionSearch={this.positionSearch} />
                         </div>
@@ -559,7 +567,7 @@ class Roster extends Component {
                             <PlayerDisplayRow player={player} key={i} addDropPlayer={this.addDropPlayer} evenOrOddRow={i % 2} />
                         ))}
                     </div>
-                    <div className='rosterCol'>
+                    <div className='rosterCol largeScreenShow'>
                         <div className='searchRow'>
                             Player Search
                             <PlayerSearch playerSearch={this.state.playerSearch} handleChange={this.handleChange} customPlayerSearch={this.customPlayerSearch} />
