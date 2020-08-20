ad5662c306066b32fe09067239ff5c7e5d5216b1
Smushed <smushedcode@gmail.com>
2020-08-20 14:22:51 -0500

    Updating to transparent logo

--- client/src/componenets/NavBar/NavBar.js
+++ client/src/componenets/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import * as Routes from '../../constants/routes';
 import SignOutButton from './SignOutButton';
 
-import ElimLogo from './ElimLogo.jpg';
+import ElimLogo from './ElimLogo.png';
 import './navBarStyle.css';
 
 
