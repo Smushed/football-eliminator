b1076d96c83cda0fef0ff952453d73d1460ab996
Smushed <smushedcode@gmail.com>
2020-08-26 16:04:19 -0500

    Basic group list working

--- client/src/componenets/GroupPage/GroupSelect.js
+++ client/src/componenets/GroupPage/GroupSelect.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withAuthorization } from '../Session';
 import axios from 'axios';
+
+import './groupStyle.css';
+
 
 class GroupSelect extends Component {
     constructor(props) {
@@ -12,17 +15,63 @@ class GroupSelect extends Component {
     componentDidMount() {
         this.getGroupList()
     };
+
     getGroupList = async () => {
         const groupList = await axios.get(`/api/getGroupList`);
-        console.log(groupList)
-        this.setState({ groupList })
+        this.setState({ groupList: groupList.data })
     };
+
+    joinGroup = async (groupId) => {
+        console.log(groupId)
+    };
+
     render() {
         return (
-            <div>Select Group</div>
+            <Fragment>
+                <div className='joinGroupHeader'>
+                    <div className='col1'>
+                        <div>
+                            Group Name
+                        </div>
+                        <small>
+                            Hover for Userlist
+                        </small>
+                    </div>
+                    <div className='col2'>
+                        Group Description
+                    </div>
+                    <div className='col3'>som</div>
+                </div>
+                {this.state.groupList.map(group =>
+                    <div key={group.N}>
+                        <GroupRow
+                            name={group.N}
+                            desc={group.D}
+                            UL={group.UL}
+                            groupId={group.id}
+                            joinGroup={this.joinGroup}
+                        />
+                    </div>)}
+            </Fragment>
         )
     };
 };
+
+const GroupRow = (props) => (
+    <div className='joinGroupRow'>
+        <div className='col1'>
+            {props.name}
+        </div>
+        <div className='col2'>
+            {props.desc}
+        </div>
+        <div className='col3'>
+            <button className='btn btn-outline-primary' onClick={() => props.joinGroup(props.groupId)} >
+                Join
+            </button>
+        </div>
+    </div>
+);
 
 const condition = authUser => !!authUser;
 
