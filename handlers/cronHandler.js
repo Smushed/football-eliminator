05888f95b79052a4fc52d3be3619f075d641ca53
Kevin Flerlage <kflerlage@spiderrockadvisors.com>
2022-08-01 15:58:49 -0500

    Updating with timers

--- handlers/cronHandler.js
+++ handlers/cronHandler.js
@@ -7,87 +7,92 @@ const groupHandler = require(`../handlers/groupHandler`);
 const emailHandler = require(`../handlers/emailHandler`);
 const moment = require(`moment-timezone`);
 
-// Schedule a job for thge 22nd minute of each hour
+// Schedule a job for the 22nd minute of each hour
 // Doing this every hour rather than daily in case Heroku isn't working
-schedule.scheduleJob('22 * * 1,9-12 *', async function () {
-    //Need to update which week we're currently in
-    const currDate = moment.utc(new Date()).tz(`America/Chicago`).toDate();
-    console.log(`Checking start week and lock week ${currDate}`);
+schedule.scheduleJob("22 * * 1,9-12 *", async function () {
+  //Need to update which week we're currently in
+  const currDate = moment.utc(new Date()).tz(`America/Chicago`).toDate();
+  console.log(`Checking start week and lock week ${currDate}`);
 
-    const currDBWeeks = await userHandler.pullSeasonAndWeekFromDB();
+  const currDBWeeks = await userHandler.pullSeasonAndWeekFromDB();
 
-    startWeek(currDate, currDBWeeks, 1);
+  startWeek(currDate, currDBWeeks, 1);
 });
 
 // Update Scores every day at 3am Chicago time
-schedule.scheduleJob('0 9 * 1,9-12 *', async function () {
-    console.log(`Running daily score update`);
-    const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
-    await mySportsHandler.updateRoster(season);
-    await updatePlayerData(season, week);
+schedule.scheduleJob("0 9 * 1,9-12 *", async function () {
+  console.log(`Running daily score update`);
+  const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
+  await mySportsHandler.updateRoster(season);
+  await updatePlayerData(season, week);
 
-    allScheduledGames(season);
+  allScheduledGames(season);
 
-    //Rank the players
-    const clapper = await groupHandler.getGroupData(`The Clapper`);  //Default to the clapper as the 'main' group
-    const groupScore = await groupHandler.getGroupScore(clapper._id);
+  //Rank the players
+  const clapper = await groupHandler.getGroupData(`The Clapper`); //Default to the clapper as the 'main' group
+  const groupScore = await groupHandler.getGroupScore(clapper._id);
 
-    const rankedPlayers = await mySportsHandler.rankPlayers(season, week, groupScore);
-    await mySportsHandler.savePlayerRank(rankedPlayers);
+  const rankedPlayers = await mySportsHandler.rankPlayers(
+    season,
+    week,
+    groupScore
+  );
+  await mySportsHandler.savePlayerRank(rankedPlayers);
 });
 
 // Thursday and Monday games (these are in UTC)
-schedule.scheduleJob('0 0-5 * 1,9-12 2,5', async function () {
-    console.log(`Running bi-hourly Monday and Thursday game score`);
-    const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
-    updatePlayerData(season, week)
+schedule.scheduleJob("0 0-5 * 1,9-12 2,5", async function () {
+  console.log(`Running bi-hourly Monday and Thursday game score`);
+  const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
+  updatePlayerData(season, week);
 });
 
 //Update most often on Sunday
-schedule.scheduleJob('0 17-23 * 1,9-12 0', async function () {
-    console.log(`Running bi-hourly Sunday job`);
-    const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
-    updatePlayerData(season, week);
+schedule.scheduleJob("0 17-23 * 1,9-12 0", async function () {
+  console.log(`Running bi-hourly Sunday job`);
+  const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
+  updatePlayerData(season, week);
 });
 
 //Right before the Leaderboard is sent out update the ideal roster
-schedule.scheduleJob('20 9 * 1,9-12 2', async function () {
-    console.log(`Updating Ideal Roster`);
-    const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
-    groupHandler.updateAllIdealRosters(season, week);
+schedule.scheduleJob("20 9 * 1,9-12 2", async function () {
+  console.log(`Updating Ideal Roster`);
+  const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
+  groupHandler.updateAllIdealRosters(season, week);
 });
 
 //Send out the Leaderboard every Tuesday
-schedule.scheduleJob('30 9 * 1,9-12 2', async function () {
-    console.log(`Sending out the weekly email`);
-    const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
-    const groups = await groupHandler.getAllGroups();
-    for (let group of groups) {
-        if (group.N !== 'Demo Group') emailHandler.sendLeaderBoardEmail(group, season, +week);
-    }
+schedule.scheduleJob("30 9 * 1,9-12 2", async function () {
+  console.log(`Sending out the weekly email`);
+  const { season, week } = await userHandler.pullSeasonAndWeekFromDB();
+  const groups = await groupHandler.getAllGroups();
+  for (let group of groups) {
+    if (group.N !== "Demo Group")
+      emailHandler.sendLeaderBoardEmail(group, season, +week);
+  }
 });
 
 const allScheduledGames = (season) => {
-    for (let i = 1; i <= 17; i++) {
-        mySportsHandler.checkGameStarted(season, i);
-    }
-}
+  for (let i = 1; i <= 17; i++) {
+    mySportsHandler.checkGameStarted(season, i);
+  }
+};
 
 const updatePlayerData = async (season, week) => {
-    await mySportsHandler.getWeeklyData(season, week);
-    await rosterHandler.scoreAllGroups(season, week);
-    return;
+  await mySportsHandler.getWeeklyData(season, week);
+  await rosterHandler.scoreAllGroups(season, week);
+  return;
 };
 
 const startWeek = (currDate, currDBWeeks, currWeek) => {
-    for (let i = 17; i >= 0; i--) {
-        if (currDate > dates.startWeek2021[i]) {
-            currWeek = i + 1;
-            break;
-        }
+  for (let i = 17; i >= 0; i--) {
+    if (currDate > dates.startWeek2021[i]) {
+      currWeek = i + 1;
+      break;
     }
-    if (currDBWeeks.week !== currWeek) {
-        userHandler.updateCurrWeek(currWeek);
-        userHandler.updateLockWeek(currWeek - 1);
-    }
+  }
+  if (currDBWeeks.week !== currWeek) {
+    userHandler.updateCurrWeek(currWeek);
+    userHandler.updateLockWeek(currWeek - 1);
+  }
 };
--- models/MatchUps.js
+++ models/MatchUps.js
@@ -3,21 +3,24 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const MatchUpsSchema = new Schema({
-    S: { //Season
-        type: String,
-        required: true,
-        trim: true
+  S: {
+    //Season
+    type: String,
+    required: true,
+    trim: true,
+  },
+  W: {
+    //Week
+    type: Number,
+    required: true,
+  },
+  M: [
+    {
+      H: String, //Home
+      A: String, //Away
+      _id: false,
     },
-    W: { //Week
-        type: Number,
-        required: true,
-    },
-    M: [{
-        H: String, //Home
-        A: String, //Away
-        W: String,  //Weather
-        _id: false
-    }]
+  ],
 });
 
 module.exports = mongoose.model(`MatchUps`, MatchUpsSchema);
