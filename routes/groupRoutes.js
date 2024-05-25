9ca0d673fca852229205e3e367600b121c1e082b
smushed <smushedcode@gmail.com>
2024-05-25 10:43:28 -0500

    Removing a cl

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -46,7 +46,6 @@ module.exports = (app) => {
   app.get(`/api/group/details/byUser/:userId`, async (req, res) => {
     const { userId } = req.params;
     const groupInfoArray = await groupHandler.getGroupDataByUserId(userId);
-    console.log({ groupInfoArray });
     res.status(200).send(groupInfoArray);
   });
 
