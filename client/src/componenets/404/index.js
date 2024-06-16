ef7bec3a1d15447c3499901e1f0dbbfefebcf56e
smushed <smushedcode@gmail.com>
2024-06-15 22:56:41 -0500

    Updating without the logo on 404

--- client/src/componenets/404/index.js
+++ client/src/componenets/404/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { home } from '../../constants/routes';
 import PoorBaby from './Champs.jpg';
-import whiteBackground from '../../constants/elimLogos/DarkModeLargeElimLogo.png';
 import './404style.css';
 
 const FourOFour = () => {
   return (
     <div className='fourOFour'>
-      <img src={whiteBackground} />
       <div className='fourOFourText'>404 - Not Found</div>
       <div className='gloat'>
         <Link to={home}>
