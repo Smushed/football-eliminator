0c4dcaae3f1f70ab02f240adb77827ff887474d5
smushed <smushedcode@gmail.com>
2024-06-15 22:13:14 -0500

    Updating with the new whitebackground logo

--- client/src/componenets/404/index.js
+++ client/src/componenets/404/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { home } from "../../constants/routes";
-import PoorBaby from "./Champs.jpg";
-import "./404style.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { home } from '../../constants/routes';
+import PoorBaby from './Champs.jpg';
+import whiteBackground from '../../constants/elimLogos/ElimLogoNoTheWhiteBackground.png';
+import './404style.css';
 
 const FourOFour = () => {
   return (
-    <div className="fourOFour">
-      <div className="fourOFourText">404 - Not Found</div>
-      <div className="gloat">
+    <div className='fourOFour'>
+      <img src={whiteBackground} />
+      <div className='fourOFourText'>404 - Not Found</div>
+      <div className='gloat'>
         <Link to={home}>
-          <img className="ripGod" src={PoorBaby} alt="Sore Losers" />
+          <img className='ripGod' src={PoorBaby} alt='Sore Losers' />
         </Link>
       </div>
     </div>
