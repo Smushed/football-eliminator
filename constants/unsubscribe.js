eaf5467458f796e587b4b5d3a75da6ddf5c05ed3
smushed <smushedcode@gmail.com>
2024-06-16 20:21:38 -0500

    Setting up reminder emails for Thursday mornings

--- constants/unsubscribe.js
+++ constants/unsubscribe.js
@@ -1,15 +1,15 @@
 export default {
   appendHTML: (email, userId) => {
     email += `<div style='text-align: center;
-                                margin-top: 20px;'>
-                        Don't want these emails? <a href='http://www.eliminator.football/email/unsubscribe/${userId}'>Unsubscribe here</a> 
+                          margin-top: 20px;'>
+                        Don't want these emails? <a href='https://www.eliminator.football/email/unsubscribe/${userId}'>Unsubscribe here</a> 
                     </div>`;
     return email;
   },
   appendText: (email, userId) => {
     email += `
         
-        Don't want these emails? Unsubscribe here: http://www.eliminator.football/email/unsubscribe/${userId}`;
+  Don't want these emails? Unsubscribe here: https://www.eliminator.football/email/unsubscribe/${userId}`;
     return email;
   },
 };
