c7c5d396237b5bb0e45ed7ea9ec97a32d952dfb3
smushed <smushedcode@gmail.com>
2024-06-26 20:29:16 -0500

    Adding authHandler to git

---
+++ handlers/authHandler.js
@@ -0,0 +1,60 @@
+import 'dotenv/config.js';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import db from '../models/index.js';
+
+initializeApp({
+  credential: cert({
+    type: process.env.FIREBASE_type,
+    project_id: process.env.FIREBASE_project_id,
+    private_key_id: process.env.FIREBASE_private_key_id,
+    private_key: process.env.FIREBASE_private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_client_email,
+    client_id: process.env.FIREBASE_client_id,
+    auth_uri: process.env.FIREBASE_auth_uri,
+    token_uri: process.env.FIREBASE_token_uri,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.FIREBASE_client_x509_cert_url,
+    universe_domain: process.env.FIREBASE_universe_domain,
+  }),
+});
+
+const notAuthorizedError = { message: 'Unauthorized', status: 401 };
+
+const verifyTokenMiddleware = (req, res, next) => {
+  if (!req.headers.token) {
+    next();
+  } else {
+    getAuth()
+      .verifyIdToken(req.headers.token)
+      .then((decodedToken) => {
+        req.currentUser = decodedToken.email;
+        next();
+      });
+  }
+};
+
+const verifyGroupAdminByEmail = async (userEmail, groupId) => {
+  const group = await db.Group.findById(groupId, { userlist: 1 }).lean().exec();
+  const userIdList = group.userlist.map((user) => user.userId.toString());
+  const userlist = await db.User.find(
+    { _id: { $in: userIdList } },
+    { email: 1 }
+  )
+    .lean()
+    .exec();
+  const foundUser = userlist.find((user) => user.email === userEmail);
+  if (!foundUser) {
+    throw notAuthorizedError;
+  }
+};
+
+const verifyUserLoggedIn = async (userEmail) => {
+  console.log({ userEmail });
+  if (userEmail === null || userEmail === undefined || userEmail === '') {
+    throw notAuthorizedError;
+  }
+};
+
+export { verifyGroupAdminByEmail, verifyUserLoggedIn, verifyTokenMiddleware };
